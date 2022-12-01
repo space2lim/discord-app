fc9ba077d2e5c23e8c1824bbc017f9528831476e
임승주 <space2lim@naver.com>
2022-12-01 20:27:33 +0900

    FIX: correct user.js command name

--- commands/user.js
+++ commands/user.js
@@ -2,8 +2,8 @@ const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('ping')
-		.setDescription('Replies with Pong!'),
+		.setName('user')
+		.setDescription('Provides information about the user.'),
 	async execute(interaction) {
 		// interaction.user is the object representing the User who ran the command
 		// interaction.member is the GuildMember object, which represents the user in the specific guild
