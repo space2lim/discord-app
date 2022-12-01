bb32e6e925d3972f19ddc73fc3f4204f328de38f
임승주 <space2lim@naver.com>
2022-12-01 20:24:09 +0900

    FIX: ping.js and user.js is confused

--- commands/ping.js
+++ commands/ping.js
@@ -5,8 +5,6 @@ module.exports = {
 		.setName('ping')
 		.setDescription('Replies with Pong!'),
 	async execute(interaction) {
-		// interaction.user is the object representing the User who ran the command
-		// interaction.member is the GuildMember object, which represents the user in the specific guild
-		await interaction.reply(`This command was run by ${interaction.user.username}, who joined on ${interaction.member.joinedAt}.`);
+		await interaction.reply('Pong!');
 	},
 };
