e3e30d4e6c73f13e26e990c1eb1e38ea79d5ab89
임승주 <space2lim@naver.com>
2022-12-04 21:15:55 +0900

    FEAT: add deferred responses and follow-ups

--- commands/ping.js
+++ commands/ping.js
@@ -1,10 +1,15 @@
 const { SlashCommandBuilder } = require('discord.js');
+const wait = require('node:timers/promises').setTimeout;
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
 		.setDescription('Replies with Pong!'),
 	async execute(interaction) {
-		await interaction.reply('Pong!');
+		await interaction.deferReply({ ephemeral: true });
+		await wait(1000);
+		await interaction.editReply({ content: 'Secret Pong!'});
+		await wait(1000);
+		await interaction.followUp({ content: 'Pong again!', ephemeral: true});
 	},
 };
