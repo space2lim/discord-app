e6dd32942f7f76a417e61d28ede261ee584763ca
임승주 <space2lim@naver.com>
2022-12-05 19:24:52 +0900

    Style: edit reply message

--- commands/help.js
+++ commands/help.js
@@ -6,6 +6,6 @@ module.exports = {
 		.setName('help')
 		.setDescription("allnighter's Notion"),
 	async execute(interaction) {
-		await interaction.reply("[Allnighter discord.js 개발기](https://allnighter.notion.site/Discord-js-9029de00881042a68399abec14e6b32f)");
+		await interaction.reply("[Discord.js 개발기를 확인하세요](https://allnighter.notion.site/Discord-js-9029de00881042a68399abec14e6b32f)");
 	},
 };
