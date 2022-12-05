ab5a042f441b6af8c6d30a695cd125fb3aba03e6
임승주 <space2lim@naver.com>
2022-12-05 19:25:13 +0900

    Feat: localize message

--- commands/server.js
+++ commands/server.js
@@ -5,7 +5,9 @@ module.exports = {
 		.setName('server')
 		.setDescription('Provides information about the server.'),
 	async execute(interaction) {
-		// interaction.guild is the object representing the Guild in which the command was run
-		await interaction.reply(`This server is ${interaction.guild.name} and has ${interaction.guild.memberCount} members.`);
+		const locales = {
+			ko: `서버 이름: ${interaction.guild.name}\n인원: ${interaction.guild.memberCount}`
+		};
+		await interaction.reply(locales[interaction.locale] ?? `This server is ${interaction.guild.name} and has ${interaction.guild.memberCount} members.`);
 	},
 };
--- commands/user.js
+++ commands/user.js
@@ -5,8 +5,9 @@ module.exports = {
 		.setName('user')
 		.setDescription('Provides information about the user.'),
 	async execute(interaction) {
-		// interaction.user is the object representing the User who ran the command
-		// interaction.member is the GuildMember object, which represents the user in the specific guild
-		await interaction.reply(`This command was run by ${interaction.user.username}, who joined on ${interaction.member.joinedAt}.`);
+		const locales = {
+			ko: `${interaction.user}(${interaction.user.username})에 의해 실행된 커맨드\n가입일: ${interaction.member.joinedAt}`
+		};
+		await interaction.reply(locales[interaction.locale] ?? `This command was run by ${interaction.user}(${interaction.user.username}), who joined on [${interaction.member.joinedAt}].`);
 	},
 };
